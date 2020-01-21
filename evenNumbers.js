da658c2f9ae5f9b89cd4a1a831c05366eaf1075d
Zeljkos <zeljkos@ever-near.com>
2020-01-21 16:49:02 +0100

    even numbers

---
+++ evenNumbers.js
@@ -0,0 +1,13 @@
+const nums = [];
+
+function evenNumbers() {
+
+    for (let i = 1; i < 100; i++) {
+        if (i % 2 == 0) {
+            nums.push(i);
+        }
+    }
+}
+evenNumbers();
+
+console.log(nums);
