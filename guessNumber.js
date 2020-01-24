23e2612e27faec898e5c9c047b547aa75a007039
Zeljkos <zeljkos@ever-near.com>
2020-01-24 14:53:26 +0100

    last incorrect number shows you lost

--- guessNumber.js
+++ guessNumber.js
@@ -2,22 +2,28 @@ function guessNumber(from, to, numberOfTrials) {
     
     const randomNumber = Math.round(Math.random() * (to - from)) + from;
     
-    for(let i = 0; i < numberOfTrials; i++){
+    for(let i = 1; i <= numberOfTrials; i++){
         
         const yourNumber = Number(prompt(`Guess the number from ${from} to ${to}.`));
         
-        if(!yourNumber || (yourNumber < from || yourNumber > to)){
-            alert('You entered incorrect number or out of the correct interval');
-        } else {
-            if(yourNumber < randomNumber){     
-                alert('Your number is lower then the correct answer.');
-            } else if(yourNumber > randomNumber){
-                alert('Your number is higher then the correct answer.');
-            } else {
-                alert('You guessed the number.');
-                break;
+        if(yourNumber == randomNumber){
+            alert('You guessed the number!');
+            break;
+        } else if(numberOfTrials - i > 0) {
+
+            if(!yourNumber || (yourNumber < from || yourNumber > to)){
+                alert('You entered incorrect number or out of the interval');
+            } else {       
+                if(yourNumber > randomNumber){
+                    alert('Your number is higher then the correct answer.');
+                } else {
+                    alert('Your number is lower then the correct answer.');
+                }
             }
-        }
+        } else {
+            alert('You lost!')        
+            break;
+        } 
     }
 }
 
