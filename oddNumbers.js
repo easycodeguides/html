ce4b1b2478b3d95be341e50a437b96be2b16211d
Tadija Selakovic <selakovic.tadija@gmail.com>
2020-01-24 16:06:53 +0100

    program daje tri najveca broja

--- oddNumbers.js
+++ oddNumbers.js
@@ -1,13 +1,41 @@
-const nums = [];
+let randNums=[];
 
-function oddNumbers() {
+document.getElementById("showing5ran").addEventListener("click",show5ranNums);
+document.getElementById("showing3largest").addEventListener("click", show3largNums);
 
-    for (let i = 1; i < 100; i++) {
-        if (i % 2 !== 0) {
-            nums.push(i);
-        }
+function show5ranNums(){
+    for(let i=0; i<10;i++){
+        let temp=Math.floor(Math.random() * 10+1);
+    if(randNums.includes(temp)==false){
+        randNums.push(temp);
+    }else{
+        i--;
     }
+    }
+    let temp2="";
+    for(let j=0; j<10;j++){
+temp2+=randNums[j]+", ";
+    }
+    let newStr = temp2.slice(0, -2);
+    document.getElementById("5ranNums").innerHTML=newStr;
 }
-oddNumbers();
 
-console.log(nums);
+function show3largNums(){
+    let temp3=[];
+    for(let i=0;i<3;i++){
+        let largestNum=Math.max(...randNums);
+        temp3.push(largestNum);
+        for( let j = 0; j < randNums.length; j++){ 
+            if ( randNums[j] === largestNum) {
+                randNums.splice(j, 1); 
+            }
+         }
+    }
+   
+    let temp4="";
+    for(let x=0; x<3;x++){
+temp4+=temp3[x]+", ";
+    }
+    let newStr2 = temp4.slice(0, -2);
+    document.getElementById("largest3nums").innerHTML=newStr2;
+}
