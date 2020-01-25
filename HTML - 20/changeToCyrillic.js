d135020911826497aa3ed458a1d562940c1f78dc
KatarinaZivanovic <katarina@DESKTOP-OH6O236.localdomain>
2020-01-25 22:18:51 +0100

    HTML-20 Added input field and button as well as the action to perform change from latin to cyrillic

---
+++ HTML - 20/changeToCyrillic.js
@@ -0,0 +1,57 @@
+let latinLetters = document.getElementById("latin").value.toString().toLowerCase();
+
+function changing() {
+    document.getElementById("cyr").innerHTML = getCyrillic(latinLetters).toString();
+}
+
+function getCyrillic(latin) {
+    let cyrillic = latin;
+    for (let i = 0; i < latin.length; i++) {
+        if (latin[i] === "l" && latin[i+1] === "j") {
+            cyrillic[i] = "љ";
+            i++;
+        }
+        if (latin[i] === "n" && latin[i+1] === "j") {
+            cyrillic[i] = "њ";
+            i++;
+        }
+        if (latin[i] === "d" && latin[i+1] === "ž") {
+            cyrillic[i] = "џ";
+            i++;
+        }
+        cyrillic[i] = switchTo(latin.charAt(i));
+    }
+    return cyrillic;
+}
+
+function switchTo(latinCharacter) {
+    let cyrillicCharacter = latinCharacter;
+    if (latinCharacter === "a") { cyrillicCharacter = "а"}
+    if (latinCharacter === "b") { cyrillicCharacter = "б"}
+    if (latinCharacter === "v") { cyrillicCharacter = "в"}
+    if (latinCharacter === "g") { cyrillicCharacter = "г"}
+    if (latinCharacter === "d") { cyrillicCharacter = "д"}
+    if (latinCharacter === "đ") { cyrillicCharacter = "ђ"}
+    if (latinCharacter === "e") { cyrillicCharacter = "е"}
+    if (latinCharacter === "ž") { cyrillicCharacter = "ж"}
+    if (latinCharacter === "z") { cyrillicCharacter = "з"}
+    if (latinCharacter === "i") { cyrillicCharacter = "и"}
+    if (latinCharacter === "j") { cyrillicCharacter = "ј"}
+    if (latinCharacter === "k") { cyrillicCharacter = "к"}
+    if (latinCharacter === "l") { cyrillicCharacter = "л"}
+    if (latinCharacter === "m") { cyrillicCharacter = "м"}
+    if (latinCharacter === "n") { cyrillicCharacter = "н"}
+    if (latinCharacter === "o") { cyrillicCharacter = "о"}
+    if (latinCharacter === "p") { cyrillicCharacter = "п"}
+    if (latinCharacter === "r") { cyrillicCharacter = "р"}
+    if (latinCharacter === "s") { cyrillicCharacter = "с"}
+    if (latinCharacter === "t") { cyrillicCharacter = "т"}
+    if (latinCharacter === "ć") { cyrillicCharacter = "ћ"}
+    if (latinCharacter === "u") { cyrillicCharacter = "у"}
+    if (latinCharacter === "f") { cyrillicCharacter = "ф"}
+    if (latinCharacter === "h") { cyrillicCharacter = "х"}
+    if (latinCharacter === "c") { cyrillicCharacter = "ц"}
+    if (latinCharacter === "č") { cyrillicCharacter = "ч"}
+    if (latinCharacter === "š") { cyrillicCharacter = "ш"}
+    return cyrillicCharacter;
+}
