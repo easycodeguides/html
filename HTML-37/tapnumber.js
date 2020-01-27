dbcb121686ebdc489f547408508bb13e70c2bb60
hanibane <haneypearltams@gmail.com>
2020-01-27 17:24:36 +0100

    HTML-37 Fixed code

--- HTML-37/tapnumber.js
+++ HTML-37/tapnumber.js
@@ -1,82 +1,58 @@
+let randomNumber = Math.floor(Math.random() * 6);
+let colors = ['red', 'green', 'blue', "brown", "violet", "orange"];
 randomNumberFunction();
 colorTable();
+
+
+
 document.getElementById('click-this').addEventListener('click', shuffleRandomTable);
+document.getElementById('table-here').addEventListener('click', selectColors);
 
 function randomNumberFunction() {
-    let randomNumber = Math.floor(Math.random() * 6);
     let result = document.getElementById('result');
     result.innerHTML = randomNumber;
-    let colorText = ['#f00', '#0f0', '#00f', "#f00", "#0f0", "#00f"];
-    shuffle(colorText);
-    for (let i = 0; i < 6; i++) {
-        if (i == randomNumber) {
-            if (randomNumber == 0) {
-                result.innerHTML = 'Tap the <span style="color: ' + colorText[i] + '">color_0</span>';
-                console.log(randomNumber);
-            } else if (randomNumber == 1) {
-                result.innerHTML = 'Tap the <span style="color: ' + colorText[i] + '">color_1</span>';
-                console.log(randomNumber);
-            } else if (randomNumber == 2) {
-                result.innerHTML = 'Tap the <span style="color: ' + colorText[i] + '">color_2</span>';
-                console.log(randomNumber);
-            } else if (randomNumber == 3) {
-                result.innerHTML = 'Don\'t tap the <span style="color: ' + colorText[i] + '">color_3</span>';
-                console.log(randomNumber);
-            } else if (randomNumber == 4) {
-                result.innerHTML = 'Don\'t tap the <span style="color: ' + colorText[i] + '">color_4</span>';
-                console.log(randomNumber);
-            } else {
-                result.innerHTML = 'Don\'t tap the <span style="color: ' + colorText[i] + '">color_5</span>';
-                console.log(randomNumber);
-            }
-        }
-    }
-
-
-    function selectColors(event) {
-        let id = event.target.getAttribute('id');
-        if (randomNumber <= 2) {
-            if (id && id.charAt(6) == randomNumber) {
-                alert('You got it!');
-                console.log(randomNumber);
-            } else {
-                alert('You didn\'t got it!')
-            }
-        } else if (randomNumber > 2) {
-            if (id && id.charAt(6) == randomNumber) {
-                alert('You didn\'t got it!');
-                console.log(randomNumber);
-            } else {
-                alert('You got it!')
-            }
-        }
-
-    }
-
-    document.getElementById('table-here').addEventListener('click', selectColors);
-
+    shuffle(colors);
+    let randomNumberTwo = Math.floor(Math.random() *6);
+    result.innerHTML = 'Tap the <span style="color: ' + colors[randomNumberTwo] + '">'+colors[randomNumber]+'</span>';
 }
+function selectColors(event) {
+    let color = event.target.getAttribute('data-color'); // event.target.dataset.color
+    if (randomNumber <= 2) {
+        if (color == colors[randomNumber]) {
+            alert('You got it!');
+            console.log(randomNumber);
+        } else {
+            alert('You didn\'t got it!')
+        }
+    } else if (randomNumber > 2) {
+        if (color == colors[randomNumber]) {
+            alert('You didn\'t got it!');
+            console.log(randomNumber);
+        } else {
+            alert('You got it!')
+        }
+    }
+}
+
+
 function shuffleRandomTable() {
     randomNumberFunction();
     colorTable()
 }
 
-
 function colorTable() {
     let tabela = '<table>';
-    let colors = ['color_0', 'color_1', 'color_2', 'color_3', 'color_4', 'color_5'];
     shuffle(colors);
     for (let i = 0; i < 1; i++) {
         tabela += '<tr>';
         for (let j = 0; j < 6; j++) {
-            tabela += '<td id="' + colors[j] + '">' + colors[j] + '</td>';
+            tabela += '<td style="background:' + colors[j] + '" data-color="'+colors[j]+'"></td>';
         }
         tabela += '</tr>'
     }
     tabela += '</table>';
     document.getElementById('table-here').innerHTML = tabela
 }
-
 
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
