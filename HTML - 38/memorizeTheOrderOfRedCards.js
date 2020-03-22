a82108897f348ba76dfdbac032f6dc7d026a05c7
KatarinaZivanovic <katarina@DESKTOP-OH6O236.localdomain>
2020-03-22 21:05:16 +0100

    HTML-38: Added {} for IF

--- HTML - 38/memorizeTheOrderOfRedCards.js
+++ HTML - 38/memorizeTheOrderOfRedCards.js
@@ -8,11 +8,13 @@ function randomArray() {
     }
     function shuffle(array) {
         let tmp, current, top = array.length;
-        if(top) while(--top) {
+        if(top) { 
+            while(--top) {
             current = Math.floor(Math.random() * (top + 1));
             tmp = array[current];
             array[current] = array[top];
             array[top] = tmp;
+            }
         }
         return array;
     }
